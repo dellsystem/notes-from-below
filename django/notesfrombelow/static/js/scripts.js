10404803b046839419c50cc433c3821d71957509
dellsystem <ilostwaldo@gmail.com>
2018-12-14 16:05:38 +0000

    Add smooth scrolling for footnotes
    Closes #59.

--- django/notesfrombelow/static/js/scripts.js
+++ django/notesfrombelow/static/js/scripts.js
@@ -12,3 +12,19 @@ function toggleSearch() {
 };
 
 $('.ui.dropdown').dropdown();
+
+$('.footnote-backref,.footnote-ref').click(function(e) {
+    e.preventDefault();
+    // There is a colon in the ID so can't just put into $() directly
+    var dest = document.getElementById($(this).attr('href').substring(1));
+    // Only add the "highlighted" class for the actual footnote text.
+    if (dest.tagName == 'LI') {
+        $(dest).addClass('highlighted');
+        setTimeout(function () {
+            $(dest).removeClass('highlighted');
+        }, 3000)
+    }
+    var offset = $(dest).offset().top;
+    console.log(offset);
+    $('html').animate({ scrollTop: offset}, 'slow');
+});
