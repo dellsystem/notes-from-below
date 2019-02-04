90830405b07ecec201647bdc0c3fdfc881f77df4
dellsystem <ilostwaldo@gmail.com>
2019-02-04 14:01:24 -0800

    Improve ImageUploadAdmin layout
    Now showing images, dimensions in list page. Closes #65.

--- django/uploads/admin.py
+++ django/uploads/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from reversion_compare.admin import CompareVersionAdmin
 
@@ -7,8 +8,29 @@ from .models import *
 
 
 class ImageUploadAdmin(CompareVersionAdmin):
-    list_display = ['title', 'slug']
+    list_display = ['display_title', 'get_dimensions', 'show_image']
     prepopulated_fields ={'slug': ('title',),}
+
+    def display_title(self, obj):
+        to_return = '<h1 class="ui header">{title}<div class="sub header">{slug}</div></h1>'.format(
+            title=obj.title,
+            slug=obj.slug,
+        )
+        return mark_safe(to_return)
+    display_title.short_description = 'Image details'
+
+    def get_dimensions(self, obj):
+        return '{width} x {height}'.format(
+            width=obj.file.width,
+            height=obj.file.height,
+        )
+
+    def show_image(self, obj):
+        to_return = '<img src="{}" class="ui small image" />'.format(
+            obj.file.url
+        )
+        return mark_safe(to_return)
+    show_image.short_description = 'Thumbnail'
 
 
 class PdfUploadAdmin(CompareVersionAdmin):
