8ca00f9b4cc9928776e4868dcbb0ebcf7284bc03
dellsystem <ilostwaldo@gmail.com>
2019-05-29 22:37:53 +0100

    Fix bug with blog posts in sitemaps
    It was triggering a 500

--- django/blog/sitemaps.py
+++ django/blog/sitemaps.py
@@ -5,16 +5,10 @@ from blog.models import BlogPost
 
 class BlogSitemap(Sitemap):
     def priority(self, item):
-        if item.is_static:
-            return 0.4
-        else:
-            return 0.9
+        return 0.3
 
     def changefreq(self, item):
-        if item.is_static:
-            return 'never'
-        else:
-            return 'monthly'
+        return 'never'
 
     def items(self):
         return BlogPost.objects.filter(published=True).order_by('pk')
